dde7d4b9135f1e49186204987e866e5da949312d
sooh329 <glay@kakao.com>
2024-09-20 00:16:07 +0900

    네비게이션 추가

---
+++ screens/ClosetScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ClosetScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Closet Screen</Text>
+    </View>
+  );
+};
+
+
+export default ClosetScreen;
---
+++ screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Main Screen</Text>
+    </View>
+  );
+};
+
+export default MainScreen;
